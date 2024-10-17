6db2a58b134a669a2d9ee00ea88f5d198bf30498
BonjeBongo <37078653+BonjeBongo@users.noreply.github.com>
2024-10-16 20:46:35 -0400

    not homemanger

--- nixos/system.nix
+++ nixos/system.nix
@@ -18,6 +18,13 @@
     libvirtd.enable = true;
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # dconf
   programs.dconf.enable = true;
 
